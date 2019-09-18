d6668e19344c27380b03dc00f635c872d2e1824c
ssunmaster <55498018+ssunmaster@users.noreply.github.com>
2019-09-18 15:21:36 +0100

    Add files via upload

---
+++ ExerciceJS.js
@@ -0,0 +1,14 @@
+document.getElementById('message');
+document.getElementById('date');
+console.log('19/09/17');
+
+function changerImage() {
+
+	document.getElementById('img').onmouseover().innerHTML = "https://pixabay.com/fr/piscine-l-eau-bleu-turquoise-802025/";
+	
+}
+
+
+
+
+
